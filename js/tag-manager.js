d1121ace9a6ee580c69d6ce60ac2760726f2590e
Faishal Wafiq Zakiy <fwzakiy@gmail.com>
2021-08-09 12:12:10 +0700

    Update tag-manager.js

--- js/tag-manager.js
+++ js/tag-manager.js
@@ -1,20 +1,88 @@
-<script>
-  (function(a,b,c){var d=a.history,e=document,f=navigator||{},g=localStorage,
-  h=encodeURIComponent,i=d.pushState,k=function(){return Math.random().toString(36)},
-  l=function(){return g.cid||(g.cid=k()),g.cid},m=function(r){var s=[];for(var t in r)
-  r.hasOwnProperty(t)&&void 0!==r[t]&&s.push(h(t)+"="+h(r[t]));return s.join("&")},
-  n=function(r,s,t,u,v,w,x){var z="https://www.google-analytics.com/collect",
-  A=m({v:"1",ds:"web",aip:c.anonymizeIp?1:void 0,tid:b,cid:l(),t:r||"pageview",
-  sd:c.colorDepth&&screen.colorDepth?screen.colorDepth+"-bits":void 0,dr:e.referrer||
-  void 0,dt:e.title,dl:e.location.origin+e.location.pathname+e.location.search,ul:c.language?
-  (f.language||"").toLowerCase():void 0,de:c.characterSet?e.characterSet:void 0,
-  sr:c.screenSize?(a.screen||{}).width+"x"+(a.screen||{}).height:void 0,vp:c.screenSize&&
-  a.visualViewport?(a.visualViewport||{}).width+"x"+(a.visualViewport||{}).height:void 0,
-  ec:s||void 0,ea:t||void 0,el:u||void 0,ev:v||void 0,exd:w||void 0,exf:"undefined"!=typeof x&&
-  !1==!!x?0:void 0});if(f.sendBeacon)f.sendBeacon(z,A);else{var y=new XMLHttpRequest;
-  y.open("POST",z,!0),y.send(A)}};d.pushState=function(r){return"function"==typeof d.onpushstate&&
-  d.onpushstate({state:r}),setTimeout(n,c.delay||10),i.apply(d,arguments)},n(),
-  a.ma={trackEvent:function o(r,s,t,u){return n("event",r,s,t,u)},
-  trackException:function q(r,s){return n("exception",null,null,null,null,r,s)}}})
-  (window,"UA-108437472-1",{anonymizeIp:true,colorDepth:true,characterSet:true,screenSize:true,language:true});
-</script>
+(function (context, trackingId, options) {
+    const history = context.history;
+    const doc = document;
+    const nav = navigator || {};
+    const storage = localStorage;
+    const encode = encodeURIComponent;
+    const pushState = history.pushState;
+    const typeException = 'exception';
+    const generateId = () => Math.random().toString(36);
+    const getId = () => {
+        if (!storage.cid) {
+            storage.cid = generateId()
+        }
+        return storage.cid;
+    };
+    const serialize = (obj) => {
+        var str = [];
+        for (var p in obj) {
+            if (obj.hasOwnProperty(p)) {
+                if(obj[p] !== undefined) {
+                    str.push(encode(p) + "=" + encode(obj[p]));
+                }
+            }
+        }
+        return str.join("&");
+    };
+    const track = (
+        type, 
+        eventCategory, 
+        eventAction, 
+        eventLabel, 
+        eventValue, 
+        exceptionDescription, 
+        exceptionFatal
+    ) => {
+        const url = 'https://www.google-analytics.com/collect';
+        const data = serialize({
+            v: '1',
+            ds: 'web',
+            aip: options.anonymizeIp ? 1 : undefined,
+            tid: trackingId,
+            cid: getId(),
+            t: type || 'pageview',
+            sd: options.colorDepth && screen.colorDepth ? `${screen.colorDepth}-bits` : undefined,
+            dr: doc.referrer || undefined,
+            dt: doc.title,
+            dl: doc.location.origin + doc.location.pathname + doc.location.search,
+            ul: options.language ? (nav.language || "").toLowerCase() : undefined,
+            de: options.characterSet ? doc.characterSet : undefined,
+            sr: options.screenSize ? `${(context.screen || {}).width}x${(context.screen || {}).height}` : undefined,
+            vp: options.screenSize && context.visualViewport ? `${(context.visualViewport || {}).width}x${(context.visualViewport || {}).height}` : undefined,
+            ec: eventCategory || undefined,
+            ea: eventAction || undefined,
+            el: eventLabel || undefined,
+            ev: eventValue || undefined,
+            exd: exceptionDescription || undefined,
+            exf: typeof exceptionFatal !== 'undefined' && !!exceptionFatal === false ? 0 : undefined,
+        });
+
+        if(nav.sendBeacon) {
+            nav.sendBeacon(url, data)
+        } else {
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            xhr.send(data);
+        }
+    };
+    const trackEvent = (category, action, label, value) => track('event', category, action, label, value);
+    const trackException = (description, fatal) => track(typeException, null, null, null, null, description, fatal);
+    history.pushState = function (state) {
+        if (typeof history.onpushstate == "function") {
+            history.onpushstate({ state: state });
+        }
+        setTimeout(track, options.delay || 10);
+        return pushState.apply(history, arguments);
+    }
+    track();
+    context.ma = {
+        trackEvent,
+        trackException
+    }
+})(window, "UA-108437472-1", {
+    anonymizeIp: true,
+    colorDepth: true,
+    characterSet: true,
+    screenSize: true,
+    language: true
+});
